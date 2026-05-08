0cac81a89850fec6913e215b8efe89958728cb6c
sayoo7 <92317602+sayoo7@users.noreply.github.com>
2026-05-08 23:52:34 +0200

    rsync backup script with logger handling

---
+++ bash/2/Backup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# This script requires SSH key-based authentication  
+SOURCE="student@serverb:/etc/config"
+DEST="/configbackup/"
+
+if ! mkdir -p "$DEST"; then
+    logger -p user.err "BACKUP FAIL: Nie można utworzyć katalogu $DEST"
+    exit 1
+fi
+# Sync using rsync
+rsync -avz -e "ssh -o BatchMode=yes" --delete "$SOURCE" "$DEST"
+# rsync check
+if [ $? -eq 0 ]; then
+    logger -p user.info "BACKUP SUCCESS"
+else
+    # logger crit error
+    logger -p user.crit "BACKUP FATAL ERROR FOR: $SOURCE"
+    exit 1
+fi
